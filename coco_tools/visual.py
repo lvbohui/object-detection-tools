7353fdce7550470f96b35f3f214d727303eaf33f
bohui_lv <bohui_lv@foxitsoftware.cn>
2022-06-13 13:45:03 +0800

    fix coord bug

--- coco_tools/visual.py
+++ coco_tools/visual.py
@@ -9,9 +9,9 @@ from pycocotools import coco
 # Visualization of COCO data-set
 parser = argparse.ArgumentParser()
 parser.add_argument("--annotation_file", type=str, 
-                    default="rotated_annotations.json")
+                    default="rotated_doorwindow_valid.json")
 parser.add_argument("--image_dir", type=str,
-                    default="output/augmentation")
+                    default="dataset/rotated_doorwindow/valid")
 parser.add_argument("--output_dir", type=str,
                     default="output/anno_visual")
 args = parser.parse_args()
@@ -23,6 +23,7 @@ coco = coco.COCO(args.annotation_file)
 images = coco.imgs
 image2anno = coco.imgToAnns
 
+cnt = 0
 for image_id in images.keys():
     image_file_name = images[image_id]["file_name"]
     image = cv2.imread(os.path.join(args.image_dir, image_file_name))
@@ -31,6 +32,10 @@ for image_id in images.keys():
     annos = image2anno[image_id]
     for anno in annos:
         bbox = anno["bbox"]
-        x1, y1, x2, y2 = bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]
+        x1, y1, x2, y2 = int(bbox[0]), int(bbox[1]), int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3])
         image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), thickness=2)
     cv2.imwrite(os.path.join(args.output_dir, image_file_name), image)
+
+    cnt += 1
+    if cnt == 10:
+        break
